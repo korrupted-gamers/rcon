ed80c3a11c76495b1f7eacf032b1f946009a9978
Chris Grimmett <chris@grimtech.net>
2018-09-26 14:33:15 -0700

    add ListPlayers command after Auth

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const Rcon = require('rcon');
-const handler = require('./handler');
+//const handler = require('./handler');
 
 
 const rcon = new Rcon(
@@ -18,17 +18,22 @@ rcon.on('auth', () => {
     if (!authenticated) {
         authenticated = true;
         console.log('Authenticated!');
+        var buffer = "ListPlayers";
+        rcon.send(buffer);
+        buffer = "";
     }
+
 }).on('response', str => {
     if (str) {
-        let channel = discord.channels.find('id', config.discord.channel);
-        channel.send(str)
-            .then(message => console.log(message.content))
-            .catch(console.error);
+        console.log(`>> response: ${str}`)
+        // let channel = discord.channels.find('id', config.discord.channel);
+        // channel.send(str)
+        //     .then(message => console.log(message.content))
+        //     .catch(console.error);
     }
 }).on('end', () => {
     rcon.connect();
+    console.log('endo')
 });
 
-
-discord.login(config.discord.token);
+rcon.connect();
