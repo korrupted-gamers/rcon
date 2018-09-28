872a55218ce6b206952bcfecf3654f96f515abfd
Chris Grimmett <chris@grimtech.net>
2018-09-27 21:10:58 -0700

    formatting

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
+const nedb = require('nedb');
 const Rcon = require('rcon');
 const handler = require('./handler');
+
 
 // pull the admins file when the program starts
 handler.fileops.pullAdminsFile();
@@ -9,70 +11,65 @@ handler.fileops.pullAdminsFile();
 
 
 const rcon = new Rcon(
-    process.env.RCON_HOST,
-    process.env.RCON_PORT,
-    process.env.RCON_PASSWORD,
-    {
-      "tcp": true,
-      "challenge": false
-    }
+  process.env.RCON_HOST,
+  process.env.RCON_PORT,
+  process.env.RCON_PASSWORD, {
+    "tcp": true,
+    "challenge": false
+  }
 );
 
 let authenticated = false;
 rcon.on('auth', () => {
-    if (!authenticated) {
-        authenticated = true;
-        console.log('Authenticated!');
-		const mainTimer = setInterval(() => {
-			var buffer = "ListPlayers";
-			rcon.send(buffer);
-			buffer = "";
-		}, 10000);
-    }
+  if (!authenticated) {
+    authenticated = true;
+    console.log('Authenticated!');
+    const mainTimer = setInterval(() => {
+      var buffer = "ListPlayers";
+      rcon.send(buffer);
+      buffer = "";
+    }, 10000);
+  }
 
 }).on('response', str => {
-    if (str) {
-        console.log(`>> response: ${str}`)
-        // let channel = discord.channels.find('id', config.discord.channel);
-        // channel.send(str)
-        //     .then(message => console.log(message.content))
-        //     .catch(console.error);
+  if (str) {
+    console.log(`>> response: ${str}`)
 
-		if (str.indexOf('----- Active Players -----') !== -1) {
-			console.log('activ players found in response');
+    if (str.indexOf('----- Active Players -----') !== -1) {
+      console.log('activ players found in response');
 
-			const usersRegex = /ID: (\d+) \| SteamID: (.+) \| Name: (.+)\n/g;
+      const usersRegex = /ID: (\d+) \| SteamID: (.+) \| Name: (.+)\n/g;
 
 
-			var match = usersRegex.exec(str);
-			while (match !== null) {
-				console.log(match)
-				console.log(`MATCHO:::::: ${match[1]}, ${match[2]}, ${match[3]}`)
+      var match = usersRegex.exec(str);
+      while (match !== null) {
+        console.log(match)
+        console.log(`MATCHO:::::: ${match[1]}, ${match[2]}, ${match[3]}`)
 
-				handler.fileops.addSpawner({
-					message: {
-						name: match[3],
-						steamID: match[2]
-					},
-					rcon
-				});
+        handler.fileops.addSpawner({
+          message: {
+            name: match[3],
+            steamID: match[2]
+          },
+          rcon
+        });
 
-				handler.commands.AdminBroadcast({
-					message: `Welcome ${name}! Next round you will have access to AdminCreateVehicle!`,
-					rcon
-				})
+        handler.commands.AdminBroadcast({
+          message: `Welcome ${name}! Next round you will have access to AdminCreateVehicle!`,
+          rcon
+        })
 
-				match = usersRegex.exec(str);
+        match = usersRegex.exec(str);
 
-			}
+      }
 
-
-		}
 
     }
+
+  }
 }).on('end', () => {
-    rcon.connect();
-    console.log('endo')
+  rcon.connect();
+  console.log('endo')
 });
 
 rcon.connect();
